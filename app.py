e89aed9ed2d529deaffb5423b4694c98350a2fe5
Kasheena Mulla <69148069+kasheena@users.noreply.github.com>
2024-03-13 12:18:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import pandas as pd
 
+def read_file(file_path, sheet_name):
+    if file_path.name.endswith('.xlsx'):
+        if sheet_name:
+            df = pd.read_excel(file_path, sheet_name=sheet_name)
+        else:
+            # If no sheet selected, read the first sheet by default
+            df = pd.read_excel(file_path, sheet_name=0)
+    else:
+        st.error("Unsupported file format. Please upload an Excel file for DataFrame 1.")
+        return None
+    return df
+
 def read_csv(file_path):
     if file_path.name.endswith('.csv'):
         df = pd.read_csv(file_path)
     else:
-        st.error("Unsupported file format. Please upload a CSV file.")
+        st.error("Unsupported file format. Please upload a CSV file for DataFrame 2.")
         return None
     return df
 
@@ -13,22 +25,22 @@ def main():
     st.title("Data Validation App")
 
     st.sidebar.header("Upload Files")
-    uploaded_file1 = st.sidebar.file_uploader("Upload CSV file for DataFrame 1", type=["csv"])
+    uploaded_file1 = st.sidebar.file_uploader("Upload Excel file for DataFrame 1", type=["xlsx", "xls"])
     uploaded_file2 = st.sidebar.file_uploader("Upload CSV file for DataFrame 2", type=["csv"])
 
     if uploaded_file1 and uploaded_file2:
-        # Read DataFrame 1
-        df1 = read_csv(uploaded_file1)
-
-        # Read DataFrame 2 and select 'PCL code' column
+        selected_sheet = st.sidebar.selectbox("Select sheet", [""] + pd.ExcelFile(uploaded_file1).sheet_names)
+        df1 = read_file(uploaded_file1, selected_sheet)
         df2 = read_csv(uploaded_file2)
-        df2 = df2[['PCL code']]
 
         if df1 is not None and df2 is not None:
+            # Select only 'PCL code' column from DataFrame 2
+            df2 = df2[['PCL code']]
+
             # Compare DataFrames to find records in DataFrame 1 that don't exist in DataFrame 2
             df3 = pd.merge(df1, df2, how='left', left_on='PCL code', right_on='PCL code', indicator=True)
             df3 = df3[df3['_merge'] == 'left_only'].drop('_merge', axis=1)
-            
+
             st.header("DataFrame 1")
             st.table(df1)
 
