d11976633db2233b6783ba99bbddfdadf0493238
Kasheena Mulla <69148069+kasheena@users.noreply.github.com>
2024-03-13 12:25:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-def read_file(file_path, sheet_name):
-    if file_path.name.endswith(('.xlsx', '.xls')):
-        if sheet_name:
-            df = pd.read_excel(file_path, sheet_name=sheet_name, skiprows=9)
-        else:
-            # If no sheet selected, read the first sheet by default
-            df = pd.read_excel(file_path, sheet_name=0, skiprows=9)
+def read_excel_file(file_path, sheet_name):
+    if sheet_name:
+        df = pd.read_excel(file_path, sheet_name=sheet_name, skiprows=9)
     else:
-        st.error("Unsupported file format. Please upload an Excel file.")
-        return None
+        df = pd.read_excel(file_path, sheet_name=0, skiprows=9)
+    return df
+
+def read_csv_file(file_path):
+    df = pd.read_csv(file_path)
     return df
 
 def main():
@@ -18,10 +17,11 @@ def main():
 
     st.sidebar.header("Upload Files")
     uploaded_file1 = st.sidebar.file_uploader("Upload Excel file for DataFrame 1", type=["xlsx", "xls"])
+    uploaded_file2 = st.sidebar.file_uploader("Upload CSV file for DataFrame 2", type=["csv"])
 
     if uploaded_file1:
         selected_sheet = st.sidebar.selectbox("Select sheet", [""] + pd.ExcelFile(uploaded_file1).sheet_names)
-        df1 = read_file(uploaded_file1, selected_sheet)
+        df1 = read_excel_file(uploaded_file1, selected_sheet)
 
         if df1 is not None:
             # Select only columns 9 to 30
@@ -30,8 +30,15 @@ def main():
             # Convert non-numeric data to string
             df1 = df1.applymap(lambda x: str(x) if not pd.api.types.is_numeric_dtype(x) else x)
 
-            st.header("Selected Columns from DataFrame 1")
+            st.header("DataFrame 1")
             st.table(df1)
+
+    if uploaded_file2:
+        df2 = read_csv_file(uploaded_file2)
+
+        if df2 is not None:
+            st.header("DataFrame 2")
+            st.table(df2)
 
 if __name__ == "__main__":
     main()
