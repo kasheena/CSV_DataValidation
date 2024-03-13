0fe6c37e52910dda8ffba8cf013ecb31db9b6042
Kasheena Mulla <69148069+kasheena@users.noreply.github.com>
2024-03-13 14:09:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def main():
             headers = text_values_list_1_1[:3]  # Get first three records as headers
             for header in headers:
                 if header == headers[0]:  # If it's the first header
-                    input_dict["Sales"] = [value for value in text_values_list_1_1[3:] if 'C' in value]
+                    input_dict["Sales"] = [value for value in text_values_list_1_1[3:] if 'C' in value and 'New' not in text_values_list_1_1[3:]]
                 elif header == headers[1]:  # If it's the second header
-                    input_dict["Gross Profit"] = [value for value in text_values_list_1_1[3:] if 'E' in value]
+                    input_dict["Gross Profit"] = [value for value in text_values_list_1_1[3:] if 'E' in value and 'New' not in text_values_list_1_1[3:]]
                 elif header == headers[2]:  # If it's the third header
-                    input_dict["Incentives"] = [value for value in text_values_list_1_1[3:] if 'G' in value]
+                    input_dict["Incentives"] = [value for value in text_values_list_1_1[3:] if 'G' in value and 'New' not in text_values_list_1_1[3:]]
                 else:
                     input_dict["Line Label"] = [value for value in text_values_list_1_1[3:] if value == 'New']
 
